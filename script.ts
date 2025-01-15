a021d7df6f0f4115bd5de64878b06b9f6258b29e
Banin Fawwaz Hadin <baninfawwazhadin@gmail.com>
2025-01-15 09:06:26 +0700

    feat: tambah commit count

--- script.ts
+++ script.ts
@@ -27,7 +27,7 @@ const endDate = new Date('2025-12-27')
 
 function randomCommits(type: 'dark' | 'light'): number {
   return type === 'dark'
-    ? Math.floor(Math.random() * 3) + 12
+    ? Math.floor(Math.random() * 3) + 13
     : Math.floor(Math.random() * 3) + 2
 }
 
